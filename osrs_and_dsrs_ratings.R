b5bf83c5e622590a6028e32d7e3da1e687f03a21
Matthew Bender <benderm@gmail.com>
2015-03-01 21:15:56 -0700

    .04 heavier DSRS over OSRS
    score: 0.55677

--- osrs_and_dsrs_ratings.R
+++ osrs_and_dsrs_ratings.R
@@ -30,10 +30,10 @@ colnames(matches)[8] <- "highIdOSRS"
 matches <- cbind(matches, merged3["DSRS"])
 colnames(matches)[9] <- "highIdDSRS"
 
-matches$calculated <- 0.50 + (matches$lowIdOSRS - matches$highIdOSRS)*0.019 + (matches$lowIdDSRS - matches$highIdDSRS)*0.022
+matches$calculated <- 0.50 + (matches$lowIdOSRS - matches$highIdOSRS)*0.018 + (matches$lowIdDSRS - matches$highIdDSRS)*0.023
 matches$calculated[matches$calculated > 1] <- 1
 matches$calculated[matches$calculated < 0] <- 0
 
 
 submit <- data.frame(id = matches$id, pred = matches$calculated)
-write.csv(submit, file = "Output/osrs_and_dsrs_ratings_2.csv", row.names = FALSE, quote=FALSE)
+write.csv(submit, file = "Output/osrs_and_dsrs_ratings_3.csv", row.names = FALSE, quote=FALSE)
